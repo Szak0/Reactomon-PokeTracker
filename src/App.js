10036b7d847a06adbe2ec02b37967e3e9de62301
Szak0 <sattilabence@gmail.com>
2020-09-23 21:19:21 +0200

    Using switch and route module to manipulate paths

--- src/App.js
+++ src/App.js
@@ -26,22 +26,18 @@ const App = () => {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className='app-container'>
+          <Switch>
+            <Route path={'/'} exact component={() => <Pokemons items={characters} />} />
+            <Route path={'/pokemons'} component={() => <Pokemons items={characters} />} />
+            <Route path='/types' component={Types} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
